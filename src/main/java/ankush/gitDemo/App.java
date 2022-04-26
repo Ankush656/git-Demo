47c5930bf7de5ccf21c693b426cc70b82a9023a3
Ankush Kumar <ankushjkkumar@gmail.com>
2022-04-26 15:45:05 +0530

    added git-demo to local repo

---
+++ src/main/java/ankush/gitDemo/App.java
@@ -0,0 +1,13 @@
+package ankush.gitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
